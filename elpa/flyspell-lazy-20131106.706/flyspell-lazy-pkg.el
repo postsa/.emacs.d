d888fa088105220e74d4b5f69a014fe38e6fcd33
Spencer Post <s.post1337@gmail.com>
2014-02-28 15:41:30 -0700

    added aspell and orgmode

---
+++ elpa/flyspell-lazy-20131106.706/flyspell-lazy-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flyspell-lazy" "20131106.706" "Improve flyspell responsiveness using idle timers" (quote nil))
--- init.el
+++ init.el
@@ -1,28 +1,64 @@
+;;=======================================================
 (load-theme 'wombat t)
 (add-to-list 'load-path "~/.emacs.d/")
+
 (when (>= emacs-major-version 24)
   (require 'package)
   (package-initialize)
-  (add-to-list 'package-archives '("melpa" .
-    "http://melpa.milkbox.net/packages/") t)
-  )
+  ;;melpa
   (add-to-list 'package-archives 
-    '("marmalade" .
-      "http://marmalade-repo.org/packages/") t)
+	       '("melpa" .
+		 "http://melpa.milkbox.net/packages/") t)
+  )
+;;color theme it
+(add-to-list 'package-archives 
+	     '("marmalade" .
+	       "http://marmalade-repo.org/packages/") t)
+;;load org-mode
+(add-to-list 'package-archives 
+	     '( "org" . 
+		"http://orgmode.ord/elpa") t)
+;;=======================================================
+
+;;=======================================================
+ ;;everything mardkown
 (autoload 'octave-mode "octave-mod" nil t)
-          (setq auto-mode-alist
-                (cons '("\\.m$" . octave-mode) auto-mode-alist))
+(setq auto-mode-alist
+      (cons '("\\.m$" . octave-mode) 
+	    auto-mode-alist))
 (autoload 'markdown-mode "markdown-mode.el" nil t)
-(add-to-list 'auto-mode-alist '("\\.text" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.text" . 
+				markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown" . 
+				markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md" . 
+				markdown-mode))
+;;=======================================================
+
+;;=======================================================
+;;aspell ispell stuff
+(setq defualt-ispell-program-name "aspell")
+;;=======================================================
+
+;;=======================================================
+;;switch buffer function
 (defun my-switch-to-other-buffer ()
   "Switch to other buffer"
   (interactive)
   (switch-to-buffer (other-buffer)))
+;;=======================================================
 
+;;=======================================================
+;; keybinding to switch buffer
 (global-set-key (kbd "M-b") 'my-switch-to-other-buffer)
+;;org mode bindings
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+;;=======================================================
 
+;;=======================================================
+;;catch org invisible edits
+(setq org-catch-invisible-edits 1)
 ;;prevent the startup screen from opening
 (setq inhibit-startup-message t)
 ;;line by line scrolling
@@ -47,3 +83,4 @@
 (scroll-bar-mode -1)
 ;;get rid of the menu bar
 (menu-bar-mode -1)
+;;=======================================================
