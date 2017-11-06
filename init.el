bccfd843f09f61e2bd9848404f3ff837ebd0fa23
Spencer Post <spost@fanatics.com>
2017-11-06 11:39:39 -0700

    re added the custom set vars

--- init.el
+++ init.el
@@ -194,3 +194,17 @@
 ;;use spaces instead of tabs
 (setq-default indent-tabs-mode nil)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (company-go exec-path-from-shell magit neotree company-jedi helm-company yasnippet company hlinum markdown-mode helm-git helm-projectile projectile ample-theme smartparens moe-theme darktooth-theme helm-swoop helm-gtags gruvbox-theme helm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
